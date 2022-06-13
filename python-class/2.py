0c59a1f94fddbc41ec9ce768d132aa4f74e703e5
cr <kcl4419@gmail.com>
2022-06-13 15:12:35 +0900

    create  file

---
+++ python-class/2.py
@@ -0,0 +1,5 @@
+n=int(input())
+
+for i  in range(n):
+    a ,b= int(input().split())
+    print(a)
---
+++ python-class/3.py
@@ -0,0 +1,6 @@
+num = list()
+
+for i in range(5):
+   a=input('점수를 입력해주세요 : ')
+   num.append(a)
+
